e93570272934ee856aed182418d77a6ad58dc0f7
Vitinnn <111204838+BIackDemon@users.noreply.github.com>
2024-11-12 09:24:25 -0300

    Update main.js

--- tri3 tra005/assets/main.js
+++ tri3 tra005/assets/main.js
@@ -1,49 +1,43 @@
-const galleryImageModal = document.querySelector('.gallery-image-modal');
-const galleryBtClose = galleryImageModal.querySelector('.bt-close');
-const galleryBtNext = galleryImageModal.querySelector('.bt-next');
-const galleryBtPrev = galleryImageModal.querySelector('.bt-prev'); // Botão para imagem anterior
-const galleryContent = galleryImageModal.querySelector('.content');
-const imgs = document.querySelectorAll('.gallery img');
+const galleryImageModal = document.querySelector('.gallery-image-modal')
+const galleryBtClose = galleryImageModal.querySelector('.bt-close')
+const galleryBtNext = galleryImageModal.querySelector('.bt-next')
+const galleryBtPrev = galleryImageModal.querySelector('.bt-prev')
+const galleryContent = galleryImageModal.querySelector('.content')
+const imgs = document.querySelectorAll('.gallery img')
 
-let nextImg = null;
-let prevImg = null;
+let nextImg = null
+let prevImg = null
 
-// Fecha a modal ao clicar no botão de fechar
 galleryBtClose.addEventListener('click', () => {
-  galleryImageModal.close();
-});
+  galleryImageModal.close()
+})
 
-// Avança para a próxima imagem, se houver uma próxima imagem
 galleryBtNext.addEventListener('click', () => {
-  galleryImageModal.close();
+  galleryImageModal.close()
   if (nextImg) {
-    nextImg.click();
+    nextImg.click()
   }
-});
+})
 
-// Volta para a imagem anterior, se houver uma anterior
 galleryBtPrev.addEventListener('click', () => {
-  galleryImageModal.close();
+  galleryImageModal.close()
   if (prevImg) {
-    prevImg.click();
+    prevImg.click()
   }
-});
+})
 
-// Abre a modal com a imagem selecionada e define a próxima e a anterior
 imgs.forEach(img => {
   img.addEventListener('click', () => {
-    // Define a próxima imagem, se houver
+    
     nextImg = img.parentNode.nextElementSibling 
       ? img.parentNode.nextElementSibling.querySelector('img') 
-      : null;
-
-    // Define a imagem anterior, se houver
+      : null
+    
     prevImg = img.parentNode.previousElementSibling 
       ? img.parentNode.previousElementSibling.querySelector('img') 
-      : null;
-
-    // Atualiza o conteúdo da modal com a imagem clicada
-    galleryContent.innerHTML = `<img src="${img.src}">`;
-    galleryImageModal.showModal();
-  });
-});
+      : null
+    
+    galleryContent.innerHTML = `<img src="${img.src}">`
+    galleryImageModal.showModal()
+  })
+})
